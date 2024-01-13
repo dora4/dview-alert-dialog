831e12cca50fdc6224f00fa6c3ebea28c15a33ac
dora <dora924666990@gmail.com>
2024-01-13 23:14:20 +0800

    新增11个语种的翻译

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -36,7 +36,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = rootProject.project.name
-                version = "1.4"
+                version = "1.5"
             }
         }
     }
